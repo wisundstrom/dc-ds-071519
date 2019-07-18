2ef360d36293c0f76acf95d55d20a80479493bf7
Will Sundstrom <will.a.sundstrom@gmail.com>
2019-07-18 12:06:52 -0400

    This is the excercise from class today. COMMIT TO GIT MORE IT WILL BE IMPORTANT LATER

--- 1-Module/1-Section/day_4_lecture_1_libraries_numpy/temperizer.py
+++ 1-Module/1-Section/day_4_lecture_1_libraries_numpy/temperizer.py
@@ -2,11 +2,31 @@
 
 
 def convert_f_to_c(temperature_f):
-    """Convert Fahrenheit to Celsius."""
-    temperature_c = (temperature_f - 32) * (5/9)
-    return temperature_c
+	temperature_c = (temperature_f - 32) * (5/9)
+	return temperature_c
 
 
 def convert_c_to_f(temperature_c):
-    """Convert Celsius to Fahrenheit."""
-    pass
+	f=temperature_c*(9/5)+32
+	return f
+
+def convert_k_to_c(temperature_k):
+	c=temperature_k-270
+	return c
+
+def convert_c_to_k(temperature_c):
+	k=temperature_c+270
+	return k
+
+def convert_k_to_f(temperature_k):
+	c=convert_k_to_c(temperature_k)
+	f=convert_c_to_f(c)
+	return f
+
+def convert_f_to_k(temperature_f):
+	c=convert_f_to_c(temperature_f)
+	k=convert_c_to_k(c)
+	return k
+
+
+
