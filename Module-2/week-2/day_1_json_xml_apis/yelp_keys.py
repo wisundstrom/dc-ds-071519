aba534306bea28227be3f38f227ad6d69c46e8bc
Will Sundstrom <will.a.sundstrom@gmail.com>
2019-08-06 12:33:59 -0400

    commiting inclass to my fork

--- Module-2/week-2/day_1_json_xml_apis/yelp_keys.py
+++ Module-2/week-2/day_1_json_xml_apis/yelp_keys.py
@@ -1,11 +1,11 @@
 """stores the yelp api client id and api key in a separate file for security and best practice"""
 
 def return_api_key():
-"""returns api key for yelp api"""
-    api_key = ''"
+    """returns api key for yelp api"""
+    api_key = 'LDsncygQ7e2nargpNAtDIbZ7ZcXWO-_bjKHM_LGHOYrhHjpNuKozQuwad383w2Jcs8m0az7SZwsTk8OJxQHyM78vTqKWfCdHjimWCKg4dOUABRXaTsTasdtN3WhIXXYx'
     return api_key
 
 def return_client_id():
-"""returns client id for yelp api"""
-    client_id = ''
+    """returns client id for yelp api"""
+    client_id = 'wBNrt1w_wrltClpFpkCB7Q'
     return client_id
